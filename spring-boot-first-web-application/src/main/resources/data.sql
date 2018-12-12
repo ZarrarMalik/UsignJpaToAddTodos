231977bcd8d55dd043076282f48b0de8228aeb49
zarrar <zarrarmalik81@gmail.com>
2018-12-12 15:17:56 +0000

    Using data.sql to populate jpa database instead of static list

---
+++ spring-boot-first-web-application/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into TODO
+values(10001, 'Learn Spring Boot', false, sysdate(), 'zarrar');
+insert into TODO
+values(10002, 'Learn Angular JS', false, sysdate(), 'zarrar');
+insert into TODO
+values(10003, 'Learn Amazon WebServices', false, sysdate(), 'zarrar');
